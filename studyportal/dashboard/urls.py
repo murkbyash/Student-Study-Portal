b5fb386648d6305d39eac4c8c11ee8ba0c514c84
Ashish Uike <79639276+murkbyash@users.noreply.github.com>
2022-09-06 03:28:55 +0530

    change settings

--- studyportal/dashboard/urls.py
+++ studyportal/dashboard/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('notes/', views.notes)
+    path('', views.home, name = "home"),
+    path('notes/', views.notes, name = "notes"),
+    path('delete_note/<int:pk>', views.delete_note, name = "delete-note"),
 ]
--- studyportal/dashboard/views.py
+++ studyportal/dashboard/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . forms import *
+from django.views import generic
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
-    form = NotesForm()
-    notes = Notes.objects.filter(user = request.user)
-    context = {'notes':notes, 'form': form}
-    return render(request,'dashboard/notes.html', context)
+     if request.method == "POST":
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user = request.user, title = request.POST['title'], description = request.POST['description'])
+            notes.save()
+        messages.success(request,f" Notes added from {request.user.username} successfully!")
+    
+     else:
+        form = NotesForm()
+     form = NotesForm()
+     notes = Notes.objects.filter(user = request.user)
+     context = {"notes":notes, "form": form}
+     return render(request,"dashboard/notes.html",context)
+
+
+
+def delete_note(request, pk = None):
+    Notes.objects.get(id=pk).delete()
+    return redirect('notes')
