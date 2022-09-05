88b58220af6e59277deafdd4a86bdd7a8ed1d7fa
Ashish Uike <79639276+murkbyash@users.noreply.github.com>
2022-09-06 00:07:17 +0530

    new model added

--- studyportal/dashboard/urls.py
+++ studyportal/dashboard/urls.py
@@ -1,7 +1,8 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name = "home"),
-    path('notes/', views.notes, name = "notes")
+    path('', views.home),
+    #path('notes/', views.notes)
 ]
