7b542470213d91a370e77af9a66c8ba0f035cfa2
Ashish Uike <79639276+murkbyash@users.noreply.github.com>
2022-09-06 17:42:50 +0530

    youtube section created

--- studyportal/dashboard/views.py
+++ studyportal/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from . forms import *
 from django.views import generic
+from youtubesearchpython import VideosSearch
 from django.contrib import messages
 
 # Create your views here.
@@ -94,3 +95,40 @@ def delete_homework(request,pk = None):
 
      Homework.objects.get(id =pk).delete()
      return redirect('homework')
+
+
+def youtube(request):
+    if(request.method == 'POST'):
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        video = VideosSearch(text, limit = 10)
+        result_list = []
+        for i in video.result()['result']:
+            result_dict ={
+                'input': text,
+                'title': i['title'],
+                'duration': i['duration'],
+                'thumbnail': i['thumbnails'][0]['url'],
+                'channel': i['channel']['name'],
+                'link': i['link'],
+                'views': i['viewCount']['short'],
+                'published': i['publishedTime'],
+            }
+            desc = ''
+            if i['descriptionSnippet']:
+                for j in i['descriptionSnippet']:
+                    desc += j['text']
+            result_dict['description'] = desc
+            result_list.append(result_dict)
+
+            context = {
+
+                'form': form,
+                'results': result_list,
+            }
+        return render(request, 'dashboard/youtube.html',context)
+    else:
+        form = DashboardForm()
+    form = DashboardForm()
+    context = {'form': form}
+    return render(request, 'dashboard/youtube.html',context)
