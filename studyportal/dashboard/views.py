c924b763d3dbd37acce0624776b8cf39456e2f0b
Ashish Uike <79639276+murkbyash@users.noreply.github.com>
2022-09-06 17:07:55 +0530

    models updated

--- studyportal/dashboard/views.py
+++ studyportal/dashboard/views.py
@@ -75,3 +75,22 @@ def homework(request):
             'form': form
             }
    return render(request, "dashboard/homework.html", context)
+
+
+
+def update_homework(request,pk = None):
+    
+    homework = Homework.objects.get(id = pk)
+    if homework.is_finished == True:
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect('homework')
+
+
+
+def delete_homework(request,pk = None):
+
+     Homework.objects.get(id =pk).delete()
+     return redirect('homework')
