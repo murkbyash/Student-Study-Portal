6d047de48d2182bb6c5de9a3671b3fc39e63e9cb
Ashish Uike <79639276+murkbyash@users.noreply.github.com>
2022-09-07 15:50:13 +0530

    books sections completed

--- studyportal/dashboard/views.py
+++ studyportal/dashboard/views.py
@@ -3,6 +3,7 @@ from . forms import *
 from django.views import generic
 from youtubesearchpython import VideosSearch
 from django.contrib import messages
+import requests
 
 # Create your views here.
 def home(request):
@@ -179,3 +180,37 @@ def update_todo(request,pk = None):
 def delete_todo(request,pk = None):
     todo = Todo.objects.get(id=pk).delete()
     return redirect('todo')
+
+
+def books(request):
+    if(request.method == 'POST'):
+        form = DashboardForm(request.POST)
+        text = request.POST['text']
+        url = "https://www.googleapis.com/books/v1/volumes?q="+ text
+        r = requests.get(url)
+        answer = r.json()
+        result_list = []
+        for i in range(10):
+            result_dict ={
+                'title': answer['items'][i]['volumeInfo']['title'],
+                'subtitle': answer['items'][i]['volumeInfo'].get('subtitle'),
+                'description': answer['items'][i]['volumeInfo'].get('description'),
+                'count': answer['items'][i]['volumeInfo'].get('pageCount'),
+                'categories': answer['items'][i]['volumeInfo'].get('categories'),
+                'rating': answer['items'][i]['volumeInfo'].get('pageRating'),
+                'thumbnail': answer['items'][i]['volumeInfo'].get('imageLinks').get('thumbnail'),
+                'preview': answer['items'][i]['volumeInfo'].get('previewLink')
+            }
+            
+            result_list.append(result_dict)
+
+            context = {
+
+                'form': form,
+                'results': result_list,
+            }
+        return render(request, 'dashboard/books.html',context)
+    else:
+       form = DashboardForm()
+    context = {"form":form}
+    return render(request, 'dashboard/books.html',context)
