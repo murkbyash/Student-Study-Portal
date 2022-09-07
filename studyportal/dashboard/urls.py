341d931ead3960f6ee8eca62347fd7354f7203b8
Ashish Uike <79639276+murkbyash@users.noreply.github.com>
2022-09-07 16:17:12 +0530

    conversion section completed

--- studyportal/dashboard/urls.py
+++ studyportal/dashboard/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     path('books', views.books, name = 'books'),
     path('dictionary/', views.dictionary, name = "dictionary"),
     path('wiki/', views.wiki, name = "wiki"),
+     path('conversion/', views.conversion, name = 'conversion'),
 ]
